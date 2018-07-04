883bc8f66a9f459fe8974bdd30d48b57a06263d5
“Fernando <starla.frr@gmail.com>
2018-07-04 12:30:35 -0500

    update selector

--- to-do-list/src/containers/Layout/Layout.js
+++ to-do-list/src/containers/Layout/Layout.js
@@ -6,25 +6,14 @@ import classes from './Layout.css';
 import axios from 'axios';
 
 //arreglo para los items de los checkbox de acuerdo al número de ID´s que tiene el endpoint
-const itemsCheck = [
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-    // 'ID TWO',
-    // 'ID THREE',
-    // 'ID FOUR',
-    // 'ID FIVE',
-    // 'ID SIX',
-    // 'ID SEVEN',
-    // 'ID EIGTH',
-    // 'ID NINE',
-    // 'ID TEN'
-];
 
 class Layout extends Component {
     constructor(props) {
         super(props)
         this.state = {
             data: [],
-            newData: []
+            newData: [],
+            selectedNumber: {}
         }
     }
 
@@ -37,7 +26,6 @@ class Layout extends Component {
                 //console.log("response setState", response)
             })
     };
-
     // componente que se creará antes de montar la aplicación
     componentWillMount ()  {
         this.selectedCheckboxes = new Set();
@@ -45,72 +33,57 @@ class Layout extends Component {
 
     // funcion para marcar o desmarcar cada casilla que el usuario seleccione
     toggleCheckbox = label => {
-        //console.log(label)
         if (this.selectedCheckboxes.has(label)) {
             this.selectedCheckboxes.delete(label);
         } else {
             this.selectedCheckboxes.add(label);
         }
+        this.setState({selectedCheckboxes: this.selectedCheckboxes})
     }
-
-    // función que se detonará con el evento onSubmit del botón "Check"
-    handleFormSubmit = formSubmitEvent => {
-        formSubmitEvent.preventDefault(); // evitará que se recargue la página
-
-        for (const checkbox of this.selectedCheckboxes) { // itera los checkbox para registrar cada elemento al que el usuario seleccione
-            console.log("check seleccionado", checkbox);
-            const newData = {...this.state.data}; // generando una variable con una copia de la data, para no mutarla en el state
-            const dataCard = this.state.data; // guardando el estado de la data en una nueva variable
-            let  userData = [ ]; // variable para agregar la información del usuario de acuerdo a la condición
-            dataCard.map(item => {
-                //console.log(item)
-                const user = item.userId; // guardando el userId para hacer la comparación con el checkbox
-                //console.log('user',user)
-                if (user === checkbox) {
-                    console.log("si son iguales")
-                    userData.push(dataCard)
-                    newData = dataCard; // intentando actualizar el estado...
-                    console.log("newdata", newData);
-                    console.log("push", user);
-                    //console.log("dataCard", dataCard);
-                }
-            })
-        };
-    };
 
     // funcion para definir las propiedades de cada checkbox
     createCheckbox = label => (
         <Checkbox
             label={label} // es el texto que se muestra junto a la casilla y proviene del arreglo itemsCheck
-            handleCheckboxChange={this.toggleCheckbox} // llama a la funcion que va a marcar o desmarcar la casilla 
+            handleCheckboxChange={this.toggleCheckbox} // llama a la funcion que va a marcar o desmarcar la casilla
             key={label} // key para identificar cada check
         />
     )
 
     // iterar el arreglo de items para crear cada check que se encuentre dentro del arreglo
-    createCheckboxes = () => (
-        itemsCheck.map(this.createCheckbox)
-    )
+    createCheckboxes = () => {
+        /*Aqui la creacion del Menu dinamico*/
+        const idMenu = this.state.data.map((ID) => {
+            return ID.userId;
+        })
+        //Aplica eliminacion de datos duplicados
+        const menu = this.unique(idMenu);
+        return menu.map(this.createCheckbox)
+    }
+
+    //Elimina datos duplicados de un Array
+    unique = (a) => {
+        return a.filter(function (value, index, self) {
+            return self.indexOf(value) === index;
+          });
+    }
 
     render(){
+        let datas = [];
         const data = this.state.data.map(data => {
-            //console.log("data mapeo", data)
-            return <Card key={data.id} title={data.title} userId={data.userId} body={data.body} />
+            for (let item of this.selectedCheckboxes){
+                item === data.userId ? datas.push(<Card key={data.id} title={data.title} userId={data.userId} body={data.body} />) : null
+            }
         });
 
         return (
             <div className="Layout">
-
                 <div className="List">
-                    <form onSubmit={this.handleFormSubmit}>
                         {this.createCheckboxes()}
-                        <button className="" type="submit">Select</button>
-                    </form>
                 </div>
                 <div className="wrapperCard">
-                   {data}
+                   {datas}
                 </div>
-
                 <Footer></Footer>
             </div>
         )
